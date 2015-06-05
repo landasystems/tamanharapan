d122a6051fffe0b1381f5da6aa46cc1494f3e2f4
x1n73ct <x1n73ct@x1n73ct>
2015-06-05 17:58:51 +0700

    Merge origin/master
    Conflicts:
    	frontend/controllers/ArticleController.php

--- frontend/views/article/sarpras.php
+++ frontend/views/article/sarpras.php
@@ -1,5 +1,15 @@
 <?php
 $this->title = 'Sarana dan Prasarana';
+
+use common\models\ArticleCategory;
+use common\models\Article;
+use common\models\User;
+use yii\data\Pagination;
+use yii\widgets\LinkPager;
+use yii\data\ActiveDataProvider;
+use yii\web\UrlManager;
+
+$session = Yii::$app->session;
 ?>
 <div class="j-menu-container"></div>
 
@@ -26,128 +36,28 @@ $this->title = 'Sarana dan Prasarana';
         <div class="container">
             <div class="row j-masonry b-col-default-indent">
                 <div class="masonry-gridSizer col-xs-12 col-md-4"></div>
-                <div class="j-masonry-item col-xs-12 col-md-4">
-                    <div class="b-blog-preview">
-                        <div class="b-blog-preview__img view view-sixth">
-                            <img data-retina="" src="<?= Yii::$app->homeUrl?>/images/sarpras/kantin.jpg" alt="" height="250">
-                            <div class="b-item-hover-action f-center mask">
-                                <div class="b-item-hover-action__inner">
-                                    
-                                </div>
-                            </div>
-                        </div>
-                        <div class="b-blog-preview__content b-diagonal-line-bg-light">
-                            <div class="b-blog-preview__content-padding_box">
-                                <p class="f-blog-preview__content-title f-primary-b"><a href="#">Kantin Sekolah</a></p>
-                                <p class="f-blog-preview__content-date f-primary-it"><?= date('d F Y')?></p>
-                                <p class="b-blog-preview__content-text f-blog-preview__content-text">Kantin sekolah yang berada di dalam sekolah dengan makanan yang selalu segar. </p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
                 
-                 <div class="j-masonry-item col-xs-12 col-md-4">
-                    <div class="b-blog-preview">
-                        <div class="b-blog-preview__img view view-sixth">
-                            <img data-retina="" src="<?= Yii::$app->homeUrl?>/images/sarpras/new-taman.jpg" alt="" height="250">
-                            <div class="b-item-hover-action f-center mask">
-                                <div class="b-item-hover-action__inner">
-                                    
-                                </div>
-                            </div>
-                        </div>
-                        <div class="b-blog-preview__content b-diagonal-line-bg-light">
-                            <div class="b-blog-preview__content-padding_box">
-                                <p class="f-blog-preview__content-title f-primary-b"><a href="#">Taman Hijau</a></p>
-                                <p class="f-blog-preview__content-date f-primary-it"><?= date('d F Y')?></p>
-                                <p class="b-blog-preview__content-text f-blog-preview__content-text">Taman hijau dan asri serta nyaman membuat susana menjadi teduh. </p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-               
-                <div class="j-masonry-item col-xs-12 col-md-4">
-                    <div class="b-blog-preview">
-                        <div class="b-blog-preview__img view view-sixth">
-                            <img data-retina="" src="<?= Yii::$app->homeUrl?>/images/sarpras/labkom.jpg" alt="" height="250">
-                            <div class="b-item-hover-action f-center mask">
-                                <div class="b-item-hover-action__inner">
-                                    
-                                </div>
-                            </div>
-                        </div>
-                        <div class="b-blog-preview__content b-diagonal-line-bg-light">
-                            <div class="b-blog-preview__content-padding_box">
-                                <p class="f-blog-preview__content-title f-primary-b"><a href="#">Lab. Komputer</a></p>
-                                <p class="f-blog-preview__content-date f-primary-it"><?= date('d F Y')?></p>
-                                <p class="b-blog-preview__content-text f-blog-preview__content-text">Lab Komputer dengan fasilitas lengkap guna mendukung siswa belajar. </p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                 
-                <div class="j-masonry-item col-xs-12 col-md-4">
-                    <div class="b-blog-preview">
-                        <div class="b-blog-preview__img view view-sixth">
-                            <img data-retina="" src="<?= Yii::$app->homeUrl?>/images/sarpras/labfisika.jpg" alt="" height="250">
-                            <div class="b-item-hover-action f-center mask">
-                                <div class="b-item-hover-action__inner">
-                                   
-                                </div>
-                            </div>
-                        </div>
-                        <div class="b-blog-preview__content b-diagonal-line-bg-light">
-                            <div class="b-blog-preview__content-padding_box">
-                                <p class="f-blog-preview__content-title f-primary-b"><a href="#">Laboratorium Fisika</a></p>
-                                <p class="f-blog-preview__content-date f-primary-it"><?= date('d F Y')?></p>
-                                <p class="b-blog-preview__content-text f-blog-preview__content-text">Lab. Fisika lengkap beserta alat-alat fisika yang memadai berguna untuk mendukung siswa praktikum. </p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="j-masonry-item col-xs-12 col-md-4">
-                    <div class="b-blog-preview">
-                        <div class="b-blog-preview__img view view-sixth">
-                            <img data-retina="" src="<?= Yii::$app->homeUrl?>/images/sarpras/mading.jpg" alt="" height="250">
-                            <div class="b-item-hover-action f-center mask">
-                                <div class="b-item-hover-action__inner">
-                                    
-                                </div>
-                            </div>
-                        </div>
-                        <div class="b-blog-preview__content b-diagonal-line-bg-light">
-                            <div class="b-blog-preview__content-padding_box">
-                                <p class="f-blog-preview__content-title f-primary-b"><a href="#">Majalah Dinding</a></p>
-                                <p class="f-blog-preview__content-date f-primary-it"><?= date('d F Y')?></p>
-                                <p class="b-blog-preview__content-text f-blog-preview__content-text">Majalah Dinding yang selalu update berita dan pengumuman untuk siswa yang di kelolah oleh para siswa sendiri. </p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                 <div class="j-masonry-item col-xs-12 col-md-4">
-                    <div class="b-blog-preview">
-                        <div class="b-blog-preview__img view view-sixth">
-                            <img data-retina="" src="<?= Yii::$app->homeUrl?>/images/sarpras/new-uks.jpg" alt="" height="250">
-                            <div class="b-item-hover-action f-center mask">
-                                <div class="b-item-hover-action__inner">
-                                    
-                                </div>
-                            </div>
-                        </div>
-                        <div class="b-blog-preview__content b-diagonal-line-bg-light">
-                            <div class="b-blog-preview__content-padding_box">
-                                <p class="f-blog-preview__content-title f-primary-b"><a href="#">Ruang UKS</a></p>
-                                <p class="f-blog-preview__content-date f-primary-it"><?= date('d F Y')?></p>
-                                <p class="b-blog-preview__content-text f-blog-preview__content-text">Ruang UKS dengan perlatan P3k lengkap berguna buat penangan pertolongan pertama untuk siswa yang lagi sakit. </p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                 <?php
+                $perpage = isset($_GET['per-page']) ? $_GET['per-page'] : 6;
+                $page = isset($_GET['page']) ? $_GET['page'] : 1;
+                $offset = ($perpage * $page) + ($perpage * -1);
+                $articles = Article::find()
+                        ->where(array(
+                            'publish' => 1,
+                            'article_category_id' => '13'
+                        ))
+//                        ->orderBy('created DESC')
+//                        ->limit($perpage)
+                        ->offset($offset)
+                        ->all();
+
+                foreach ($articles as $isi):
+                    ?>
                 
                 <div class="j-masonry-item col-xs-12 col-md-4">
                     <div class="b-blog-preview">
                         <div class="b-blog-preview__img view view-sixth">
-                            <img data-retina="" src="<?= Yii::$app->homeUrl?>/images/sarpras/new-rkepsek2.jpg" alt="" height="250">
+                            <img data-retina="" src="<?php echo (!empty($isi->primary_image)) ? Yii::$app->homeUrl . 'images/article/' . $isi->primary_image : Yii::$app->homeUrl . 'images/700x700-noimage.jpg' ?>" alt="" height="250">
                             <div class="b-item-hover-action f-center mask">
                                 <div class="b-item-hover-action__inner">
                                     
@@ -156,135 +66,20 @@ $this->title = 'Sarana dan Prasarana';
                         </div>
                         <div class="b-blog-preview__content b-diagonal-line-bg-light">
                             <div class="b-blog-preview__content-padding_box">
-                                <p class="f-blog-preview__content-title f-primary-b"><a href="#">Ruang Kepala Sekolah</a></p>
-                                <p class="f-blog-preview__content-date f-primary-it"><?= date('d F Y')?></p>
-                                <p class="b-blog-preview__content-text f-blog-preview__content-text">Ruang Kepala Sekolah baru yang menunjang aktivitas pendidikan. </p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="j-masonry-item col-xs-12 col-md-4">
-                    <div class="b-blog-preview">
-                        <div class="b-blog-preview__img view view-sixth">
-                            <img data-retina="" src="<?= Yii::$app->homeUrl?>/images/sarpras/newrtunggu.jpg" alt="" height="250">
-                            <div class="b-item-hover-action f-center mask">
-                                <div class="b-item-hover-action__inner">
-                                    
-                                </div>
-                            </div>
-                        </div>
-                        <div class="b-blog-preview__content b-diagonal-line-bg-light">
-                            <div class="b-blog-preview__content-padding_box">
-                                <p class="f-blog-preview__content-title f-primary-b"><a href="#">Ruang Tunggu</a></p>
-                                <p class="f-blog-preview__content-date f-primary-it"><?= date('d F Y')?></p>
-                                <p class="b-blog-preview__content-text f-blog-preview__content-text">Ruang Tunggu yang nyaman berfungsi supaya para tamu bisa nyaman dan tenang. </p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-               
-                <div class="j-masonry-item col-xs-12 col-md-4">
-                    <div class="b-blog-preview">
-                        <div class="b-blog-preview__img view view-sixth">
-                            <img data-retina="" src="<?= Yii::$app->homeUrl?>/images/sarpras/newrtu.jpg" alt="" height="250">
-                            <div class="b-item-hover-action f-center mask">
-                                <div class="b-item-hover-action__inner">
-                                    
-                                </div>
-                            </div>
-                        </div>
-                        <div class="b-blog-preview__content b-diagonal-line-bg-light">
-                            <div class="b-blog-preview__content-padding_box">
-                                <p class="f-blog-preview__content-title f-primary-b"><a href="#">Ruang Tata Usaha</a></p>
-                                <p class="f-blog-preview__content-date f-primary-it"><?= date('d F Y')?></p>
-                                <p class="b-blog-preview__content-text f-blog-preview__content-text">Ruang Tata Usaha dilengkapi dengan mesin fotokopi agar siswa dapat mencetak di sekolah </p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-               
-                <div class="j-masonry-item col-xs-12 col-md-4">
-                    <div class="b-blog-preview">
-                        <div class="b-blog-preview__img view view-sixth">
-                            <img data-retina="" src="<?= Yii::$app->homeUrl?>/images/sarpras/newbasket.jpg" alt="" height="250">
-                            <div class="b-item-hover-action f-center mask">
-                                <div class="b-item-hover-action__inner">
-                                   
-                                </div>
-                            </div>
-                        </div>
-                        <div class="b-blog-preview__content b-diagonal-line-bg-light">
-                            <div class="b-blog-preview__content-padding_box">
-                                <p class="f-blog-preview__content-title f-primary-b"><a href="#">Lapangan Basket</a></p>
-                                <p class="f-blog-preview__content-date f-primary-it"><?= date('d F Y')?></p>
-                                <p class="b-blog-preview__content-text f-blog-preview__content-text">Fasilitas Lapangan Basket yang luas dan bersih. </p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="j-masonry-item col-xs-12 col-md-4">
-                    <div class="b-blog-preview">
-                        <div class="b-blog-preview__img view view-sixth">
-                            <img data-retina="" src="<?= Yii::$app->homeUrl?>/images/sarpras/newtoilet.jpg" alt="" height="250">
-                            <div class="b-item-hover-action f-center mask">
-                                <div class="b-item-hover-action__inner">
-                                    <div class="b-item-hover-action__inner-btn_group">
-                                        <a href="blog_detail_left_slidebar.html" class="b-btn f-btn b-btn-light f-btn-light info"><i class="fa fa-link"></i></a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="b-blog-preview__content b-diagonal-line-bg-light">
-                            <div class="b-blog-preview__content-padding_box">
-                                <p class="f-blog-preview__content-title f-primary-b"><a href="#">Kamar Kecil / Toilet</a></p>
-                                <p class="f-blog-preview__content-date f-primary-it"><?= date('d F Y')?></p>
-                                <p class="b-blog-preview__content-text f-blog-preview__content-text">Toilet yang bersih dan nyaman. </p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="j-masonry-item col-xs-12 col-md-4">
-                    <div class="b-blog-preview">
-                        <div class="b-blog-preview__img view view-sixth">
-                            <img data-retina="" src="<?= Yii::$app->homeUrl?>/images/sarpras/perpus.jpg" alt="" height="250">
-                            <div class="b-item-hover-action f-center mask">
-                                <div class="b-item-hover-action__inner">
-                                    
-                                </div>
-                            </div>
-                        </div>
-                        <div class="b-blog-preview__content b-diagonal-line-bg-light">
-                            <div class="b-blog-preview__content-padding_box">
-                                <p class="f-blog-preview__content-title f-primary-b"><a href="#">Perpustakaan</a></p>
-                                <p class="f-blog-preview__content-date f-primary-it"><?= date('d F Y')?></p>
+                                <p class="f-blog-preview__content-title f-primary-b"><a href="<?php echo Yii::$app->urlManager->createUrl('article/' . $isi->id) ?>"><?php echo $isi->title ?></a></p>
+                               
                                 <p class="b-blog-preview__content-text f-blog-preview__content-text">
-                                Perpustakaan dengan koleksi buku lengkap.
+                                <?=$isi->content;?>
                                 </p>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div class="j-masonry-item col-xs-12 col-md-4">
-                    <div class="b-blog-preview">
-                        <div class="b-blog-preview__img view view-sixth">
-                            <img data-retina="" src="<?= Yii::$app->homeUrl?>/images/sarpras/rguru.JPG" alt="" height="250">
-                            <div class="b-item-hover-action f-center mask">
-                                <div class="b-item-hover-action__inner">
-                                    
-                                </div>
-                            </div>
-                        </div>
-                        <div class="b-blog-preview__content b-diagonal-line-bg-light">
-                            <div class="b-blog-preview__content-padding_box">
-                                <p class="f-blog-preview__content-title f-primary-b"><a href="blog_detail_right_slidebar.html">Ruang Guru</a></p>
-                                <p class="f-blog-preview__content-date f-primary-it"><?= date('d F Y')?></p>
-                                <p class="b-blog-preview__content-text f-blog-preview__content-text">
-                                Ruang Guru yang nyaman agar guru dapat nyaman menjalankan aktifitas kerja    
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                <?php
+                endforeach;
+                ?>
+                
+                
             </div>
 
         </div>
