7e214afb7a88dc3b16ac6b691429316a7508f8d9
yuliantofrandi <yuliantofrandi@laptop1>
2015-06-15 12:36:07 +0700

    Merge origin/master
    Conflicts:
    	frontend/views/article/view.php

--- backend/extensions/bootstrap/assets/js/locales/bootstrap-datepicker.th.js
+++
@@ -1,6 +0,0 @@
-¼#E‘ÿáÒ’ƒT”E9O{yÚIZ¶2ÏNånAÖËìl‰ƒµ9¬^VÂè¤[IŒÜCpoÜ‡^:	Xùóa ¶¨12e‹öZk#D6[¥ðR
-Gr]ýJ«uº–© Ë¥¬çuÈ¤·.ÿñUcy¾‘'Š!K3ùÒà²z1uHæ"pEMí¼ª VÛsX·—YÂF	i-–÷‡¬_Y¿ºíÚÞÛîâå×xçl‘Ïú,%N0X‡$C®¹â­Ì+îÅ€É–«4DÝ\1ù‹‹ö*YÚ)ó¼N?e!gA<ôá³Àø«"“Øä§ýàžÇu²Ü€Å=Pyg¸TÂ'ÿÖô­@
-ŸêÜîõãPÖ}ì=Ñ‡HTHÑ7Èø‘¨ø´‰ÍˆDƒXD$ÞÅ!S2ßo¬ž™Ì¹ü‘÷„¹;òÐ¯Ÿ
-#\2Ç‡n™½½0øÌÕ†Ø[dÔ“µ7Ä3‰þÜm¸•	ž£¢Ñs!Pƒ‘§l»6Ã-« W1PËÙñà¶j¦0C@ìâÞ_ïOëÌ]Éz+½œ½›»Ñ›’èÂ>¦:•s’«Ûtû“oÜX/F¬}ŒÐààƒ`¶”óŽIÀðO€áVh:Ðšc? žoˆ?DD¯[ÁW°=~AX ªâÁn5ìšàHQå{–’‘‰.%dª˜2|îåe¸þ+[Ø%0¹šNY‹ç$rIÜb‘yXWÏ8HŒ¢XÖIþì‘£
-e=¯Á
-9ày³ÚCS½ÝR&rñØR¥—R'_¯O¨qŠ‡OÖªJIë”•}hªnÛ²Úñ£Þ)mù{©1Ínª4Ê’(éô1«ùïo3e‰Ûºkã1îÄirïêÜã˜AÎÍ¸c>7s³ùUóF3•+Ë»,¦GÙÁ£–Ý`ï”ÇÇ€¾‹âÍhÐ7ªkˆó™¾ÒçíËòÄD±Þu+Çi=O_l}Ê5 î—\‡¼|HœõÂ€ZÈ4+=.wèåg3rÀJ8eäæöõ»·þpÈ²	ÎŒYÿ†…Ìr¥GÒÞ®	\ll5g8äåB¶Wó1´§†~z/šLP­¼â†3&R{ep”ŽJùŽlDlµ,:­‘Vtš>Ù˜…‘ Tsàì75ïHÁTô!/×ÈàšÉ¹´ÇDw˜F..úD„rå‡3i8“ŽÆ–l¯fàÙ[ˆ)r‰¿0‚_YìV®,rSéŽy8G³ÞÌÀ5LôÀž\ñ7w÷QŽr1,cÁ6N–ÅËœS#g‹1RKÖõœ)‡3Ù9œ)‡3YéÌ©‘ó
